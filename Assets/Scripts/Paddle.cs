e3b6bf4bb841fb74d7bff18b92242a341285cd9a
Smoku <tarczownikop.pl>
2020-11-02 18:34:24 +0100

    Assety

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float paddleInitialY;
+
+    private void Start()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+        paddleInitialY = this.transform.position.y;
+    }
+
+    private void Update()
+    {
+        PaddleMovement();
+
+    }
+
+  //  float mousePositionWorldX = mainCamera
+
+    private void PaddleMovement()
+    {
+        float mousePositionWorldX = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0, 0)).x;
+        this.transform.position = new Vector3(mousePositionWorldX, paddleInitialY, 0);
+    }
+ 
+}
