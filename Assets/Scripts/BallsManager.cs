6ef5e6b420bc111eb077ebe1f81c3eb1cba0c7b2
Smoku <tarczownikop.pl>
2020-11-10 18:22:16 +0100

    wa

---
+++ Assets/Scripts/BallsManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallsManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static GameManager _instance;
+
+    public static GameManager Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+
+
+    public bool IsGameStarted { get; set; }
+
+    #endregion
+
+    [SerializeField]
+
+    private Ball ballPrefab;
+
+    private Ball initialBall;
+
+    private Rigidbody2D initialBallRb;
+
+    public float initialBallSpeed = 250;
+
+    public List<Ball> Balls { get; set;  }
+
+    private void Start()
+    {
+        initialBall();
+    }
+
+    private void Update()
+    {
+        if (!GameManager.Instance.IsGameStarted)
+        {
+            Vector3 paddlePosition = Paddle.Instance.gameObject.transform.position;
+            Vector3 ballPosition = new Vector3(paddlePosition.x, paddlePosition.y + .27f, 0);
+            initialBall.transform.position = ballPosition;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                initialBallRb.isKinematic = false;
+                initialBallRb.AddForce(new Vector2(0, initialBallSpeed));
+                GameManager.Intance.IsGameStarted = true;
+
+            }
+        }
+    }
+    
+    private void InitBall()
+    {
+        Vector3 paddlePosition = Paddle.Instance.gameObject.transform.position;
+        Vector3 ballPosition = new Vector3(paddlePosition.x, paddlePosition.y + .27f, 0);
+        initialBall = Instantate(ballPrefab, startingPosition, Quaternion.identity);
+        initialBallRb = initialBall.GetComponent<RigidBody2D>();
+
+        this.Balls = new List<Ball>
+        {
+            initialBall
+        };
+    }
+
+
+
+
+}
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -6,11 +6,15 @@ public class Paddle : MonoBehaviour
 {
     private Camera mainCamera;
     private float paddleInitialY;
-
+    private float defaultPaddleWidthInPixels = 200;
+    private float defaultLeftClamp = 135;
+    private float defaultRightClamp = 410;
+    private SpriteRenderer sr;
     private void Start()
     {
         mainCamera = FindObjectOfType<Camera>();
         paddleInitialY = this.transform.position.y;
+        sr = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
@@ -23,7 +27,11 @@ public class Paddle : MonoBehaviour
 
     private void PaddleMovement()
     {
-        float mousePositionWorldX = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0, 0)).x;
+        float paddleShift = (defaultPaddleWidthInPixels - ((defaultPaddleWidthInPixels / 2) * this.sr.size.x)) / 2;
+        float leftClamp = defaultLeftClamp - paddleShift;
+        float rightClamp = defaultRightClamp - paddleShift;
+        float mousePositionPixels = Mathf.Clamp(Input.mousePosition.x, leftClamp, rightClamp);
+        float mousePositionWorldX = mainCamera.ScreenToWorldPoint(new Vector3(mousePositionPixels, 0, 0)).x;
         this.transform.position = new Vector3(mousePositionWorldX, paddleInitialY, 0);
     }
  
